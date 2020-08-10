727b07f6f3d151c8bd65e176763c2ce03a22fc41
Benoît TANI <benoit.tani@gmail.com>
2020-08-10 04:41:34 +0000

    Add print of combinaison with recursive method. With debug printf to check and understand the recursive process.

--- main.c
+++ main.c
@@ -3,19 +3,18 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
+/* For generating parenthesis */
 unsigned int open = 0;
 unsigned int close = 0;
+
 char *bopen;
 char *bclose;
-char emptys[] = "";
 static int i = 0;
 
 int checkString( char *s)
 {
 
   static unsigned int j = 0;
-  j++;
   printf("string lenght: %d, i: %d, s: %s\n", (int)strlen(s), i, s);
 
   if (strlen(s) == 0) goto end;
@@ -37,6 +36,35 @@ end:
   return i;
 }
 
+void printParenthesis(int position, int close, int open, int lenght)
+{
+  static char s[100];
+
+
+  if(close == lenght)
+  {
+    printf("%s\n", s);
+    return;
+  }
+  else
+  {
+    if(open > close)
+    {
+      printf("-- add }  pos: %d close: %d open: %d \n", position, close, open);
+      s[position] = '}';
+      printParenthesis(position+1, close+1, open, lenght);
+    }
+    if(open < lenght)
+    {
+     printf("-- add {  pos: %d close: %d open: %d \n", position, close, open);
+      s[position] = '{';
+      printParenthesis(position+1, close, open+1, lenght);
+    }
+  printf("--end pp\n");
+  }
+}
+
+
 int main(void)
 {
   char s[] = "{{{}}}";
@@ -48,4 +76,8 @@ int main(void)
 
   if(result == 0)  printf("Bracket are balanced\n");
   else printf("Bracket are NOT balanced\n");
+
+  printParenthesis(0,0,0,3);
+
+
 }
